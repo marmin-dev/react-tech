b72fe345dea494eda95ac24c7d961a2dd645bb93
Marmin <marmin.dev@gmail.com>
2023-01-22 20:02:02 +0900

    chap 13 end study about React SPA

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -3,13 +3,27 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import "./App.css";
+import Article from "./pages/Article";
+import Articles from "./pages/Articles";
+import Layout from "./pages/Layout";
+import NotFound from "./pages/NotFound";
+import Login from "./pages/Login";
+import MyPage from "./pages/MyPage";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profiles/:username" element={<Profile />} />
+      <Route element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+        <Route path="/articles" element={<Articles />}>
+          <Route path=":id" element={<Article />} />
+        </Route>
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/mypage" element={<MyPage />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
