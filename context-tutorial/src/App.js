6893ddf41706b140abd566da8131b18b588d2f20
Marmin <marmin.dev@gmail.com>
2023-01-24 23:44:33 +0900

    chap15 ended studied about context

---
+++ context-tutorial/src/App.js
@@ -0,0 +1,16 @@
+import ColorBox from "./components/ColorBox";
+import SelectColors from "./components/SelectColors";
+import { ColorProvider } from "./context/color";
+
+const App = () => {
+  return (
+    <ColorProvider>
+      <div>
+        <SelectColors />
+        <ColorBox />
+      </div>
+    </ColorProvider>
+  );
+};
+
+export default App;
