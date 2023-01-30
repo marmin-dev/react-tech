6b7341156cda7f10fcdfce0b3c10c3e1b5c6d14b
Marmin <marmin.dev@gmail.com>
2023-01-31 01:34:24 +0900

    chap22 end study about koa

--- blog/blog-backend/src/api/posts/index.js
+++ blog/blog-backend/src/api/posts/index.js
@@ -6,12 +6,8 @@ const posts = new Router();
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
 
-const post = new Router();
-
-posts.get('/', postsCtrl.read);
-posts.delete('/', postsCtrl.remove);
-posts.patch('/', postsCtrl.update);
-
-posts.use('/:id', postsCtrl.checkObjectId, post.routes());
+posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
+posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
+posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
 
 export default posts;
